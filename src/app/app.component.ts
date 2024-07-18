7f1ae23a8a61c4aa4e05e17acc3050b150db2918
rtwohey <rtwohey@ionic.io>
2024-07-18 10:16:56 -0400

    added pinch zoom and pdf viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+
+
+import { PinchZoomModule } from '@meddv/ngx-pinch-zoom';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PdfViewerModule, PinchZoomModule], // Add PinchZoomModule to imports
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-pinch-zoom';
+  pdfData: string;
+  zoom: number;
+  myPadding: number;
+
+  constructor() {
+    this.pdfData = "";
+    this.zoom = 1.0;
+    this.myPadding = 0.5;
+  }
+
+  onFileSelected() {
+    let $img: any = document.querySelector('#file');
+  
+    if (typeof (FileReader) !== 'undefined') {
+      let reader = new FileReader();
+  
+      reader.onload = (e: any) => {
+        this.pdfData = e.target.result;
+      };
+  
+      reader.readAsArrayBuffer($img.files[0]);
+    }
+  }
 }
